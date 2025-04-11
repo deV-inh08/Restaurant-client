56218fef68c6b709b020999690380cf8e99290a7
deV_inh08 <vintd2004@gmail.com>
2025-04-11 12:31:14 +0700

    code ui login page

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from '@/components/mode-toogle'
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <ModeToggle></ModeToggle>
+            <div>Home</div>
+        </>
+    )
+}
+
+export default Home
