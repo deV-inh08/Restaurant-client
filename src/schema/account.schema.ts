c8a3737a4155186483728b24c4b4711a095bc4b2
deV_inh08 <vintd2004@gmail.com>
2025-04-17 11:16:19 +0700

    code UI setting page

--- src/schema/account.schema.ts
+++ src/schema/account.schema.ts
@@ -7,12 +7,27 @@ export const AccountSchema = z.object({
     role: z.enum([Roles.Owner, Roles.Employee]),
     avatar: z.string().nullable()
 })
-
 export type AccountType = z.TypeOf<typeof AccountSchema>
+
 
 export const AccountRes = z.object({
     data: AccountSchema,
     message: z.string()
 })
-
 export type AccountResType = z.TypeOf<typeof AccountRes>
+
+
+export const ChangePasswordBody = z.object({
+    oldPassword: z.string().min(6).max(100),
+    password: z.string().min(6).max(100),
+    confirmPassword: z.string().min(6).max(100)
+})
+export type ChangePasswordBodyType = z.TypeOf<typeof ChangePasswordBody>
+
+
+export const UpdateMeBody = z.object({
+    name: z.string().trim().min(2).max(256),
+    avatar: z.string().url().optional()
+})
+
+export type UpdateMeBodyType = z.TypeOf<typeof UpdateMeBody>
