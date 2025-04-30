95afd3d0e0ebd036bb7fdfc1db62b41d45afe390
deV_inh08 <vintd2004@gmail.com>
2025-04-30 11:12:11 +0700

    code UI dishes page

--- src/constants/type.ts
+++ src/constants/type.ts
@@ -5,3 +5,13 @@ export const Roles = {
 } as const
 
 export const RoleValues = [Roles.Owner, Roles.Employee, Roles.Guest] as const
+
+
+export const DishStatus = {
+    Available: 'Available',
+    Unavailable: 'Unavailable',
+    Hidden: 'Hidden'
+} as const
+
+
+export const DishStatusValues = [DishStatus.Available, DishStatus.Unavailable, DishStatus.Hidden] as const
