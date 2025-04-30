d955cd2089a715682148a5eb4c155cd28df45187
deV_inh08 <vintd2004@gmail.com>
2025-04-30 15:02:56 +0700

    declare API table

---
+++ src/apiRequests/table.ts
@@ -0,0 +1,12 @@
+import { http } from "@/lib/http";
+import { CreateTableBodyType, TableListResType, TableResType, UpdateTableBodyType } from "@/schema/table.schema";
+
+const tableApiReq = {
+    list: () => http.get<{ payload: TableListResType }>('tables'),
+    add: (body: CreateTableBodyType) => http.post<{ payload: TableResType }>('tables', body),
+    getTable: (id: number) => http.get<{ payload: TableResType }>(`tables/${id}`),
+    updateTable: (id: number, body: UpdateTableBodyType) => http.put<{ payload: TableResType }>(`tables/${id}`, body),
+    deleteTable: (id: number) => http.delete<{ payload: TableResType }>(`tables/${id}`)
+}
+
+export default tableApiReq
