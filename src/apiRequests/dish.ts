43a2d03a08bebac937340507ddb4b62e3c6d5b7d
deV_inh08 <vintd2004@gmail.com>
2025-04-30 11:20:43 +0700

    declare API dishes

---
+++ src/apiRequests/dish.ts
@@ -0,0 +1,13 @@
+import { http } from "@/lib/http";
+import { CreateDishBodyType, DishListResType, DishResType, UpdateDishBodyType } from "@/schema/dish.schema";
+
+const dishApiRequest = {
+    // get list dishes
+    list: () => http.get<{ payload: DishListResType }>('dishes'),
+    add: (body: CreateDishBodyType) => http.post<{ payload: DishResType }>('dishes', body),
+    getDish: (id: number) => http.get<{ payload: DishResType }>(`dishes/${id}`),
+    updateDish: (id: number, body: UpdateDishBodyType) => http.put<{ payload: DishResType }>(`dishes/${id}`, body),
+    deleteDish: (id: number) => http.delete<{ payload: DishResType }>(`dishes/${id}`)
+}
+
+export default dishApiRequest
