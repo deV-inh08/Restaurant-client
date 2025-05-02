321196af87759005fe23af795dafed7417669f05
deV_inh08 <vintd2004@gmail.com>
2025-05-02 18:55:32 +0700

    update nav-items

--- src/app/(public)/nav-items.tsx
+++ src/app/(public)/nav-items.tsx
@@ -1,41 +1,78 @@
 'use client'
 
+import { Roles } from '@/constants/type'
 import useAuth from '@/hooks/useAuth'
+import { cn, handleErrorApi } from '@/lib/utils'
+import { useLogoutMutation } from '@/queries/useAuth'
+import { RoleType } from '@/types/jwt.type'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
 
-const menuItems = [
-    {
-        title: 'Món ăn',
-        href: '/menu'
-    },
-    {
-        title: 'Đơn hàng',
-        href: '/orders',
-        authRequired: true
-    },
-    {
-        title: 'Đăng nhập',
-        href: '/login',
-        authRequired: false // khong can dang nhap van hien thi
-    },
-    {
-        title: 'Quản lý',
-        href: '/manage/dashboard',
-        authRequired: true // can dang nhap de hien thi
-    }
-]
+const menuItems: {
+    title: string,
+    href: string
+    role?: RoleType[]
+    hideWhenLogin?: boolean
+}[] = [
+        {
+            title: 'Trang chủ',
+            href: '/'
+        },
+        {
+            title: 'Menu',
+            href: '/guest/menu',
+            role: [Roles.Guest]
+        },
+        {
+            title: 'Đăng nhập',
+            href: '/login',
+            hideWhenLogin: true
+        },
+        {
+            title: 'Quản lý',
+            href: '/manage/dashboard',
+            role: [Roles.Owner, Roles.Employee]
+        },
+    ]
 
 export default function NavItems({ className }: { className?: string }) {
-    const { role } = useAuth()
-    const visibleItems = menuItems.filter((item) => {
-        if (item.authRequired === false && role) return false
-        if (item.authRequired === true && !role) return false
-        return true
-    })
-
-    return visibleItems.map((item) => (
-        <Link href={item.href} key={item.href} className={className}>
-            {item.title}
-        </Link>
-    ))
+    const { role, setRole } = useAuth()
+    const logoutMutation = useLogoutMutation()
+    const router = useRouter()
+    const logout = async () => {
+        if (logoutMutation.isPending) return
+        try {
+            const result = await logoutMutation.mutateAsync()
+            toast.success(result.payload.message)
+            setRole(undefined)
+            router.push('/')
+        } catch (error) {
+            handleErrorApi({
+                error
+            })
+        }
+    }
+    return (
+        <>
+            {menuItems.map((item) => {
+                const isAuth = item.role && role && item.role.includes(role)
+                const canShow = (item.role === undefined && !item.hideWhenLogin) ||
+                    (!role && item.hideWhenLogin)
+                if (isAuth || canShow) {
+                    return (
+                        <Link href={item.href} key={item.href} className={className}>
+                            {item.title}
+                        </Link>
+                    )
+                }
+                return null
+            })}
+            {role && (
+                <span className={cn(className, 'cursor-pointer')} onClick={logout}>
+                    Đăng xuất
+                </span>
+            )}
+        </>
+    )
 }
