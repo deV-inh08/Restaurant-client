59ff8b44f17f240e13aba2be9bec0e18a62f43a8
deV_inh08 <vintd2004@gmail.com>
2025-05-03 20:51:43 +0700

    code UI manage order

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,6 +10,8 @@ import { DishStatus, OrderStatus, Roles, TableStatus } from "@/constants/type"
 import envConfig from "@/config"
 import { RoleType, TokenPayload } from "@/types/jwt.type"
 import guestApiRequest from "@/apiRequests/guest"
+import { format } from 'date-fns'
+import { BookX, CookingPot, HandCoins, Loader, Truck } from 'lucide-react'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -172,4 +174,32 @@ export const getTableLink = ({ token, tableNumber }: { token: string; tableNumbe
 
 export const decodeToken = (token: string) => {
   return jwt.decode(token) as TokenPayload
+}
+
+export function removeAccents(str: string) {
+  return str
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/đ/g, 'd')
+    .replace(/Đ/g, 'D')
+}
+
+export const simpleMatchText = (fullText: string, matchText: string) => {
+  return removeAccents(fullText.toLowerCase()).includes(removeAccents(matchText.trim().toLowerCase()))
+}
+
+export const formatDateTimeToLocaleString = (date: string | Date) => {
+  return format(date instanceof Date ? date : new Date(date), 'HH:mm:ss dd/MM/yyyy')
+}
+
+export const OrderStatusIcon = {
+  [OrderStatus.Pending]: Loader,
+  [OrderStatus.Processing]: CookingPot,
+  [OrderStatus.Rejected]: BookX,
+  [OrderStatus.Delivered]: Truck,
+  [OrderStatus.Paid]: HandCoins
+}
+
+export const formatDateTimeToTimeString = (date: string | Date) => {
+  return format(date instanceof Date ? date : new Date(date), 'HH:mm:ss')
 }
